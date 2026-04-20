9f648f2315e24e4fda785b27c37c2310026592ec
Hunter Lee <hunterclcode@gmail.com>
2026-04-19 21:24:37 -0500

    Added mcp tools

--- src/agent.py
+++ src/agent.py
@@ -3,33 +3,39 @@ import logging
 from enum import Enum
 from pocketflow import Node, Flow
 from pydantic import BaseModel, Field
+from mcp.types import Tool
 from services.mcp_adapter import MCPClient
 from services.llm_services import get_response, get_response_structured
 from prompts import DECIDE_NODE_PROMPT, RESPONSE_NODE_PROMPT
 
-decisions = ["get_schema", "generate_sql", "execute_sql", "respond"]
+def build_action_model(tools: list[Tool]):
+    print(tools)
+    Action = Enum("Action", {t.name: t.name for t in tools}, type=str)
 
-Action = Enum("Action", {d: d for d in decisions}, type=str)
+    class AgentAction(BaseModel):
+        action: Action
+        reasoning : str = Field("", description="Short reason why you chose this route, should be 1-2 sentences")
+        #args: dict= Field({}, description= "Arugments for chosen tool")
 
-class AgentAction(BaseModel):
-    action: Action
-    reasoning : str = Field("", description="Short reason why you chose this route, should be 1-2 sentences")
+    return AgentAction
 
 class decideAction(Node):
     def prep(self, shared):
-        return shared["input"]
+        return {"input": shared["input"], "action_model": shared["action_model"], "tool_context": shared["tool_context"]}
     
     def exec(self, prep_res):
-        prompt = DECIDE_NODE_PROMPT.format(TOOLS=str(decisions), CONTEXT=str(prep_res), HISTORY="")
-        return get_response_structured(prompt, AgentAction)
+        prompt = DECIDE_NODE_PROMPT.format(TOOLS=prep_res["tool_context"], CONTEXT=str(prep_res), HISTORY="")
+        return get_response_structured(prompt, prep_res["action_model"])
     
     def post(self, shared, prep_res, exec_res):
+        print("SHARED:", shared)
+        print("RESPONSE:", exec_res)
         shared["response"] = exec_res
         if not hasattr(exec_res, "action") or not "scratchpad" in shared:
             return "Error: Attribute error during decision node"
         shared["scratchpad"].append(f"Decision Node Route: [{exec_res.action}] Reason: [{exec_res.reasoning}]")
-        if hasattr(exec_res, "action") and exec_res.action == Action.respond:
-            return "respond"
+        # if hasattr(exec_res, "action") and exec_res.action == shared["action_model"].respond:
+        #     return "respond"
         return ""
 
 class responseAction(Node):
@@ -44,7 +50,10 @@ class responseAction(Node):
         return exec_res
 
 class SQLAgent():
-    def __init__(self):
+    def __init__(self, tools: list[Tool]):
+        self.action_model = build_action_model(tools)
+        self.tool_context = "\n".join(f"- {t.name}: {t.description}" for t in tools)
+
         # Nodes
         self.decide = decideAction()
         self.respond = responseAction()
@@ -56,15 +65,14 @@ class SQLAgent():
         self.flow = Flow(start=self.decide)
 
     def run(self, query: str):
-        res = self.flow.run({"input": query, "scratchpad": []})
+        res = self.flow.run({"input": query, "scratchpad": [], "action_model": self.action_model, "tool_context": self.tool_context})
         return res
 
 async def main():
-    agent = SQLAgent()
-
     async with MCPClient() as client:
         # get tools
         tools = await client.get_tools()
+        agent = SQLAgent(tools)
         for t in tools:
             print(t.name, t.description)
 

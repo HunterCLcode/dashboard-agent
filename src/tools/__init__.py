327f5aac74edf45710d91dbe65aafd59f05192e9
Hunter Lee <hunterclcode@gmail.com>
2026-05-04 21:06:33 -0500

    Refactor DF to Plotly bar graph tool and update initialization to use new argument structure

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,10 +1,17 @@
 from .reflect_tool import reflect, ReflectArgs
+from .DF_to_bar_graph_tool import DFToPlotlyBarTool, DFToPlotlyBarArgs
 
 LOCAL_TOOLS = [
     {
         "name": "reflect",
         "description": reflect.__doc__,
-        "input_schema": ReflectArgs.model_json_schema(),
+        "args_model": ReflectArgs,
         "fn": reflect,
+    },
+    {
+        "name": "dataframe to graph",
+        "description": DFToPlotlyBarTool.__doc__,
+        "args_model": DFToPlotlyBarArgs,
+        "fn": DFToPlotlyBarTool,
     }
 ]
